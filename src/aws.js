ca071ba96a674ac5c8a45b8979a0d9c56d28f63a
metaory <metaory@gmail.com>
2019-08-13 20:37:28 +0800

    fixed bug when instance doesnt have Name tag

--- src/aws.js
+++ src/aws.js
@@ -1,32 +1,20 @@
-// const { resolve, } = require('path')
 const AWS = require('aws-sdk')
-// const axios = require('axios')
-// const FS = require('./fs')
-// const exec = require('./exec')
 
 module.exports = class {
-  constructor (region) {
+  constructor(region) {
     this.ec2 = new AWS.EC2({ region })
   }
 
-  async configure (roleName) {
-    console.log('CF()')
-  }
-  async list () {
+  async list() {
     return (await this.ec2.describeInstances({
-      Filters: [{ Name: 'instance-state-name', Values: ['running', ], }, ],
-    }).promise())
+        Filters: [{ Name: 'instance-state-name', Values: ['running', ], }, ],
+      }).promise())
       .Reservations
-      .reduce((acc, cur) => {
-        acc.push(...cur.Instances.map(x => {
-          return {
-            name: x.Tags.find(t => t.Key === 'Name').Value,
-            instanceId: x.InstanceId,
-            privateIpAddress: x.PrivateIpAddress,
-            instanceLifecycle: x.InstanceLifecycle,
-          }
-        }))
-        return acc
-      }, [])
+      .reduce((acc, cur) => [...acc, ...cur.Instances.map(x => ({
+        name: (x.Tags.find(t => t.Key === 'Name') || {}).Value || '',
+        instanceId: x.InstanceId,
+        privateIpAddress: x.PrivateIpAddress,
+        instanceLifecycle: x.InstanceLifecycle,
+      }))], [])
   }
 }
